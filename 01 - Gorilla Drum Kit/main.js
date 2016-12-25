71fb1b6fdb86098363633e1f5be3dd0bed4f7cbc
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-25 15:57:46 -0500

    Removes class of playing when transition has ended. Refactor EventListener for keydown.

--- 01 - Gorilla Drum Kit/main.js
+++ 01 - Gorilla Drum Kit/main.js
@@ -1,10 +1,26 @@
 console.log("JavaScript file is wired!");
 
-window.addEventListener('keydown', function(e) {
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll('.key'); // Returns an array of all keys
+
+keys.forEach(key => key.addEventListener('transitionend', removePlayingClass));
+
+
+// ----Function to play sound when key is pressed---- //
+
+function playSound(event) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 	if(!audio) return; // stop the function from running all together
 	audio.currentTime = 0; // When the key is press, the audio rewinds to the start
 	audio.play();
 	key.classList.add('playing');
-});
+}
+
+// ----Function to remove class of playing---- //
+
+function removePlayingClass(event) {
+	if(event.propertyName !== 'transform') return // Just skip it
+	this.classList.remove('playing'); // 'this' is the div element corresponding to the pressed key
+}
