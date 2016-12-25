f47cdde1ba61ac3cd37437dcce6d3063565fc684
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-25 15:45:32 -0500

    Event listener that plays a correspoding audio when the key is pressed

--- 01 - Gorilla Drum Kit/main.js
+++ 01 - Gorilla Drum Kit/main.js
@@ -2,5 +2,9 @@ console.log("JavaScript file is wired!");
 
 window.addEventListener('keydown', function(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	console.log(audio);
+	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	if(!audio) return; // stop the function from running all together
+	audio.currentTime = 0; // When the key is press, the audio rewinds to the start
+	audio.play();
+	key.classList.add('playing');
 });
