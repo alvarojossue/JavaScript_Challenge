f269ddf9130e5306a92abd37403d2b980153c7f2
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-26 16:23:36 -0500

    Creates function to display matches

--- 06 - Type Ahead/js/main.js
+++ 06 - Type Ahead/js/main.js
@@ -2,13 +2,51 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const cities = [];
 
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+
+// ---- Fetching the data from the endpoint ---- ///
+
 fetch(endpoint) // Returns a promise
 	.then(blob => blob.json()) // First step to convert it to json, but it still a promise
 	.then(data => cities.push(...data)) // Gets the data and pushes it into the array of cities
+
+
+// ---- Event listerners for the search box ---- //
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keydown', displayMatches);
+
+
+// ---- Function to find matches in the array of cities ---- //
+
 
 function findMatches(wordToMatch, cities) {
 	return cities.filter(place => {
 		const regex = new RegExp(wordToMatch, 'gi');
 		return place.city.match(regex) || place.state.match(regex)
 	})
+}
+
+// ---- Function to display the HTML from the matches ---- //
+
+function displayMatches(){
+	const matchArray = findMatches(this.value, cities);
+	const html = matchArray.map(place => {
+		return `
+			<li>
+				<span class="name">${place.city}, ${place.state}</span>
+				<span class="population">${numberWithCommas(place.population)}</span>
+			</li>
+		`;
+	}).join('');
+	suggestions.innerHTML = html;
+}
+
+// ---- Function to format population number ---- //
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
