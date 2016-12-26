0f52d7ea71330efd4af4b66b3b8f4690321a5f39
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-26 13:33:26 -0500

    Updates inputs value on change

--- 03 - CSS Variables/js/main.js
+++ 03 - CSS Variables/js/main.js
@@ -1,1 +1,11 @@
-console.log("wired")
+const inputs = document.querySelectorAll('.controls input');
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+// ----Function to handle the changes made in the input fiels---- //
+
+function handleUpdate(){
+	const suffix = this.dataset.sizing || '';
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
