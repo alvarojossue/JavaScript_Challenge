ab5bb6faef36293abfc102385c603a5e36da340c
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-26 15:34:52 -0500

    Creates functions that toggle classes when elements are clicked

--- 05 - Flex Panel Gallery/js/main.js
+++ 05 - Flex Panel Gallery/js/main.js
@@ -1,1 +1,19 @@
-console.log("wired")
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+// ----Function to toggle the class open ---- //
+
+function toggleOpen() {
+	this.classList.toggle('open');
+}
+
+// ----Function to toggle the class open-active ---- //
+
+function toggleActive(e) {
+	if(e.propertyName.includes('flex')){
+		this.classList.toggle('open-active');
+	}
+}
