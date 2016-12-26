74b7844ddb0adb3a60f1aa3216699e47e1d0a52f
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-26 15:50:50 -0500

    Fetches the data from the provided endpoint

--- 06 - Type Ahead/js/main.js
+++ 06 - Type Ahead/js/main.js
@@ -1,3 +1,7 @@
-console.log("wired")
-
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint) // Returns a promise
+	.then(blob => blob.json()) // First step to convert it to json, but it still a promise
+	.then(data => cities.push(...data))
